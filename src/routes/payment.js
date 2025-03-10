585dc64bd41202ab5063ebe42e7532b0843d8161
keshav kumar <keshavks9810@gmail.com>
2025-03-10 21:57:39 +0530

    bug fix for env file with razorpay.js

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -36,7 +36,7 @@ paymentRouter.post("/payment/create",userAuth,async(req,res)=>{
         })
         const savedPayment = await payment.save();
         // res.send(order);
-        res.json({ ...savedPayment.toJSON(), keyId: "rzp_test_a2fY8WwpOfa0kW"});
+        res.json({ ...savedPayment.toJSON(), keyId: process.env.RAZORPAY_KEY_ID});
     }
     catch(err){
         console.log(err);
